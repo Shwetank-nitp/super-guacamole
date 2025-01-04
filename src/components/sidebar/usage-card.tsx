b275360367de390533808e960627bf608f21f70c
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2025-01-04 14:53:56 +0530

    fixed user UI

--- src/components/sidebar/usage-card.tsx
+++ src/components/sidebar/usage-card.tsx
@@ -1,7 +1,7 @@
 import { Progress } from "@/components/ui/progress";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Button } from "../ui/button";
-import { Zap } from "lucide-react";
+import { Crown, Zap } from "lucide-react";
 
 const FreeTierCard = ({ usage }: { usage: number }) => {
   return (
@@ -14,7 +14,6 @@ const FreeTierCard = ({ usage }: { usage: number }) => {
 
       <CardContent className="flex flex-col gap-y-4">
         <Progress
-          
           value={(usage / Number(process.env.NEXT_PUBLIC_USAGE_LIMIT!)) * 100}
         />
         <Button className="bg-gradient-to-tr from-purple-500 to-pink-500 text-white font-bold capitalize">
@@ -29,8 +28,8 @@ const FreeTierCard = ({ usage }: { usage: number }) => {
 const ProTierCard = () => {
   return (
     <div className="flex flex-row justify-center items-center gap-2 bg-gradient-to-tr from-purple-500 to-pink-500 rounded-lg px-4 py-2">
-      <span className="text-sm font-bold capitalize">PRO user enjoy!</span>
-      <Zap fill="white" size={16} />
+      <Crown fill="white" size={16} />
+      <span className="text-sm font-bold capitalize">Pro Member</span>
     </div>
   );
 };
