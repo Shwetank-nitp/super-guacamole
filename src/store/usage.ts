88ae27373bbd5bf84bf0dfdcd5c5b04cd41c9062
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2025-01-04 15:01:01 +0530

    update folder structure

---
+++ src/store/usage.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+
+interface Usage {
+  usage: number;
+  plan: "FREE" | "PRO" | null;
+  rank?: "SILVER" | "GOLD" | "PLATINUME";
+  valid_date?: string;
+  fetchUsage: (uid: string) => Promise<void>;
+  setUsage: (num: number, plan: "PRO" | "FREE") => void;
+}
+
+const useUsage = create<Usage>((set) => ({
+  usage: 0,
+  plan: null,
+  fetchUsage: async (userid: string) => {
+    const response = await fetch(process.env.NEXT_PUBLIC_USAGE!, {
+      method: "POST",
+      body: JSON.stringify({
+        uid_clerk: userid,
+      }),
+    });
+
+    if (!response.ok) {
+      return;
+    }
+
+    response.json().then((data) => {
+      const date = new Date(data.result?.valid_date);
+      const dateString = date.toUTCString();
+      set({
+        usage: Number(process.env.NEXT_PUBLIC_USAGE_LIMIT!) - data.result.usage,
+        plan: data.result.plan,
+        rank: data.result?.title,
+        valid_date: dateString,
+      });
+    });
+  },
+  setUsage: (num: number, plan: "PRO" | "FREE") => {
+    set({ usage: num, plan });
+  },
+}));
+
+export default useUsage;
